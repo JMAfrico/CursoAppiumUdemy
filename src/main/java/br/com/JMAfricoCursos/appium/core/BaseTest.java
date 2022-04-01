a1b6c0bad2a43fc819bad9ef28fd75aa841e14a8
JMAfrico <joao_marcossilva@hotmail.com>
2022-04-01 11:39:39 -0300

    Adicionando ResetAPP

--- src/main/java/br/com/JMAfricoCursos/appium/core/BaseTest.java
+++ src/main/java/br/com/JMAfricoCursos/appium/core/BaseTest.java
@@ -1,11 +1,17 @@
 package br.com.JMAfricoCursos.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 
 	@After
-	public void tearDown() {
+	public void finalizaTeste() {
+		DriverFactory.getDriver().resetApp();
+	} 
+	
+	@AfterClass
+	public static void finalizaClasse() {
 		DriverFactory.killDriver();
 	}
 }
